95a35e642f12bf2aecfc95ff94d2ce0080e913c2
Sriharsha Guduguntla <sguduguntla@sriharshas-mbp.lan>
2020-09-21 18:22:12 -0700

    Updating landing page repo to latest

--- js/index.js
+++ js/index.js
@@ -12,7 +12,9 @@ $(document).ready(function () {
             $.ajax({
                 url: "https://calypso-responses-api.herokuapp.com/users",
                 type: 'POST',
-                data: { email },
+                data: {
+                    email
+                },
                 success: function (data) {
                     Swal.fire({
                         title: '<strong>Success!</strong>',
@@ -26,7 +28,7 @@ $(document).ready(function () {
                     $("#error").text("Something went wrong. Try again later.");
                     $("#error").css("display", "block");
                 },
-                complete: function() {
+                complete: function () {
                     $("input").val("");
                 }
             });
@@ -34,4 +36,48 @@ $(document).ready(function () {
         }
 
     });
+
+    $("#learnMore").click(function () {
+        $('html,body').animate({
+                scrollTop: $(".features-page").offset().top - 30
+            },
+            1500);
+    });
+
+    // Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex',
+                                '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
 });
